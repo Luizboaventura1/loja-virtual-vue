c76a1f27bc76c1a1cc57d1f3901d63e76a9b1126
LuizHenrique1 <luizboaventura24@gmail.com>
2023-03-12 07:53:40 -0300

    Updates

--- js/410.3be84f80.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkloja_virtual"]=self["webpackChunkloja_virtual"]||[]).push([[410],{8410:function(n,r,t){t.r(r),t.d(r,{default:function(){return i}});var u=t(6252);function a(n,r,t,a,c,e){const o=(0,u.up)("MainProducts");return(0,u.wg)(),(0,u.j4)(o)}var c=t(8334),e={components:{MainProducts:c.Z}},o=t(3744);const s=(0,o.Z)(e,[["render",a]]);var i=s}}]);
-//# sourceMappingURL=410.3be84f80.js.map
